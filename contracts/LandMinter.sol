975d6d63807a63688f8942339f97096aaec6237d
MJsystem <audwns232@naver.com>
2022-02-17 15:21:54 +0900

    Complete royalty and batch minting

--- contracts/LandMinter.sol
+++ contracts/LandMinter.sol
@@ -3,24 +3,48 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 // We need to import the helper functions from the contract that we copy/pasted.
 import { Base64 } from "./libraries/Base64.sol";
 
-contract LandMinter is ERC721URIStorage {
+// When we need ERC721Enumerable, go import it.
+contract LandMinter is ERC721URIStorage, ERC2981, Ownable {
+  string public contractURI;
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
-    console.log("This is my NFT contract. Woah!");
+  // todo: 2 necessary args => add to deploy.js (Refer to ERC 2981 page on notion)
+  constructor(uint96 _royaltyFeesInBips, string memory _contractURI) ERC721 ("NewsLand", "NLD") {
+    setRoyaltyInfo(owner(), _royaltyFeesInBips);
+    contractURI = _contractURI;
   }
 
+// ERC2981
+  function setRoyaltyInfo(address _receiver, uint96 _royaltyFeesInBips) public onlyOwner {
+        _setDefaultRoyalty(_receiver, _royaltyFeesInBips);
+  }
+    // Figure out how to make contractURI which is a type of base 64
+  function setContractURI(string calldata _contractURI) public onlyOwner {
+        contractURI = _contractURI;
+  }
+
+
+// ERC721 minting 
   function mint() public {
     _mintNewsLand();
+  }
+
+  function batchMint(uint96 numberOfTimes) public {
+    for (uint96 i; i < numberOfTimes; i++) {
+      mint();
+    }
   }
 
   function _mintNewsLand() internal {
@@ -52,10 +76,6 @@ contract LandMinter is ERC721URIStorage {
         abi.encodePacked("data:application/json;base64,", json)
     );
 
-    console.log("\n--------------------");
-    console.log(finalTokenUri);
-    console.log("--------------------\n");
-
     _safeMint(msg.sender, newItemId);
     
     // Update your URI!!!
@@ -64,4 +84,15 @@ contract LandMinter is ERC721URIStorage {
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
   }
+
+  // The following functions are overrides required by Solidity.
+
+  function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC2981)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
